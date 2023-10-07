3f14124095d25db1fb48cc63ba8698649eff5b56
Thomas Longworth <thomaslongworth555@gmail.com>
2023-10-07 19:31:40 +0100

    add route to get country information

--- backend/index.js
+++ backend/index.js
@@ -8,5 +8,20 @@ app.use(cors())
 
 app.use(express.json())
 
+//Route to fetch country data 
+app.get("/country/:value", async(req, res)=> {
+    try {
+        const {value}=req.params
+        const response = await fetch(`https://restcountries.com/v3.1/name/${value}?fullText=true`)
+        const data = await response.json()
+        res.json(data)
+        console.log(data)
+        
+    } catch (error) {
+        console.error(error.message)
+        
+    }
+})
+
 
 app.listen(PORT)
