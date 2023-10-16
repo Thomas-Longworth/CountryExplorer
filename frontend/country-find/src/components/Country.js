9f536ba6c8d15a8296c9e77305ea76199d5dab8a
Thomas Longworth <thomaslongworth555@gmail.com>
2023-10-16 15:47:36 +0100

    add simple animation to country display div

--- frontend/country-find/src/components/Country.js
+++ frontend/country-find/src/components/Country.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import Footer from '../layout/Footer'
-
+import {motion,useAnimate} from 'framer-motion'
 import { useContext } from 'react'
 import { CountryContext } from '../Contexts/CountryContext'
 const Country = () => {
 	const{countryData} = useContext(CountryContext)
 
-
+	
 	return (
 		<>
 
@@ -20,21 +20,33 @@ const Country = () => {
 							countryData.map((each, index) => {
 								return (
 									<>
-										<div 
+										<motion.div 
+										  initial={{ opacity: 0, scale: 0.5 }}
+										  animate={{ opacity: 1, scale: 1 }}
+										  transition={{
+											duration: 0.8,
+											delay: 0.5,
+											ease: [0, 0.71, 0.2, 1.01]
+										  }}
+										
+										
+
+						
 										 className='col-auto '>
 											<div data-cy="country-name"  	key={each.name.official} class="card rounded" style={{ maxWidth: '18rem' }} >
 												<img  class="card-img-top border-bottom "src={each.flags.png} alt="country flag" />
-												<div class="card-body">
+												<div 
+												class="card-body">
 													<h5  class="card-title font-italic"><strong>{each.name.official}</strong></h5>
 												</div>
-												<ul class="list-group list-group-flush">
-													<li class="list-group-item"><strong>Region:</strong> {each.region}</li>
+												<ul 	class="list-group list-group-flush">
+													<li class="list-group-item type"><strong>Region:</strong>  {each.region}</li>
 													<li class="list-group-item"><strong>Capital:</strong> {each.capital}</li>
-													<li class="list-group-item"><strong>Population:</strong> {each.population}</li>
+													<li class="list-group-item type"><strong>Population:</strong> {each.population}</li>
 													<li class="list-group-item"><strong>Area Tm^2:</strong> {each.area}</li>
 												</ul>
 											</div>
-										</div>
+										</motion.div>
 									</>
 								)
 							})}
