b439af67bd959858f78735829868eb73c121948c
Thomas Longworth <thomaslongworth555@gmail.com>
2023-10-18 14:51:06 +0100

    add build folder for frontend

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,20 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const path = require('path')
 
 
-const PORT = 5000
 
 app.use(cors())
 
 app.use(express.json())
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 
@@ -35,5 +41,5 @@ app.get("/countries/:country", async (req, res, next) => {
   }
 });
 
-
+const PORT =  5000
 app.listen(PORT)
